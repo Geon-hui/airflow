812b666d4b2638498170822be23b8e4dbbdcc9fb
Geonhui <3634lelee@dgu.ac.kr>
2025-05-17 01:21:47 +0900

    문법 수정

--- dags/dags_python__templates.py
+++ dags/dags_python__templates.py
@@ -18,7 +18,7 @@ with DAG(
         python_t1= PythonOperator(
             task_id='python_task',
             python_callable=python_function1,
-            op_kwargs={'start_date': '{{data_interval_start | ds}}'', 'end_date': '{{data_interval_end | ds}}'}
+            op_kwargs={'start_date': '{{data_interval_start | ds}}', 'end_date': '{{data_interval_end | ds}}'}
         )
 
     @task(task_id='python_t2') # op_kwargs 내부에 존재하는 진 템플릿 변수 이용
