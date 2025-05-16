736650557530412b067775a2c3daf15f4d4aed2b
Geonhui <3634lelee@dgu.ac.kr>
2025-05-17 01:55:32 +0900

    end_date 날짜 수정

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -12,7 +12,7 @@ with DAG(
     bash_task_2 = BashOperator(
         task_id='bash_task_2', # datetime.utild은 datetime을 템플릿 변수 내에서 사용할 수 있도록 해주는 모듈
         env={'START_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul")-macros.dateutil.relativedelta.relativedelta(days=19))|ds}}', 
-             'END_DATE': '{{ (data_interval_end.in_timezone("Asia/Seoul")-macros.dateutil.relativedelta.relativedelta(days=19))|ds}}'
+             'END_DATE': '{{ (data_interval_end.in_timezone("Asia/Seoul")-macros.dateutil.relativedelta.relativedelta(days=14))|ds}}'
              },
         bash_command='echo "Start date: $START_DATE" && echo "End date: $$END_DATE"'
     )
