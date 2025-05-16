e4003a4c54430757fd86d215fdd975eb0cc6fad0
Geonhui <3634lelee@dgu.ac.kr>
2025-05-17 01:46:22 +0900

    task_id 수정

--- dags/dags_python__templates.py
+++ dags/dags_python__templates.py
@@ -16,7 +16,7 @@ with DAG(
         print(end_date)
 
         python_t1= PythonOperator(
-            task_id='python_task',
+            task_id='python_t1',
             python_callable=python_function1,
             op_kwargs={'start_date': '{{data_interval_start | ds}}', 'end_date': '{{data_interval_end | ds}}'}
         )
