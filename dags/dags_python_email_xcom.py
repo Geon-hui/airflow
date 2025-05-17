87c630a5a68ca9e9686d8ef7ab3beea16f708571
Geonhui <3634lelee@dgu.ac.kr>
2025-05-17 14:46:54 +0900

    최종수정

--- dags/dags_python_email_xcom.py
+++ dags/dags_python_email_xcom.py
@@ -4,21 +4,24 @@ import pendulum
 from airflow.decorators import task
 
 with DAG(
-    dag_id="dags_python_email_xcom",
-    schedule="30 0 * * *",
-    start_date=pendulum.datetime(2025, 5, 10, tz="Asia/Seoul"),
+    dag_id="dags_python_email_operator",
+    schedule="0 8 1 * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
+    
     @task(task_id='something_task')
-    def some_logic():
-        from random import choice
-        return choice(['success', 'failure'])
+    def some_logic(**kwargs):
+        from random import choice 
+        return choice(['Success','Fail'])
 
-    email_pull = EmailOperator(
+
+    send_email = EmailOperator(
         task_id='send_email',
         to='3634lelee@naver.com',
-        subject='{{ (data_interval_end.in_timezone("Asia/Seoul")) |ds }} some_logic 처리 결과',
-        html_content='{{ (data_interval_end.in_timezone("Asia/Seoul")) |ds }} 처리 결과는 <br> \
-                      {{ti.xcom_pull(task_ids="something_task")}} 입니다. <br>'
+        subject='{{ data_interval_end.in_timezone("Asia/Seoul") | ds }} some_logic 처리결과',
+        html_content='{{ data_interval_end.in_timezone("Asia/Seoul") | ds }} 처리 결과는 <br> \
+                    {{ti.xcom_pull(task_ids="something_task")}} 했습니다 <br>'
     )
-    some_logic() >> email_pull 
+
+    some_logic() >> send_email
