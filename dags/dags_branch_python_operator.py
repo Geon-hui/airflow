892d1f21ed6870062a7408f3bad216a2583551d2
Geonhui <3634lelee@dgu.ac.kr>
2025-05-17 14:45:55 +0900

    최종 수정

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -4,26 +4,25 @@ from airflow.operators.python import PythonOperator
 from airflow.operators.python import BranchPythonOperator
 
 with DAG(
-    dag_id='dags_base_branch_operator',
-    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
-    schedule=None,
+    dag_id='dags_branch_python_operator',
+    start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'), 
+    schedule='0 1 * * *',
     catchup=False
 ) as dag:
-    class CustomBranchOperator(BaseBranchOperator):
-        def choose_branch(self, context):
-            import random
-            print(context)
-            
-            item_lst = ['A', 'B', 'C']
-            selected_item = random.choice(item_lst)
-            if selected_item == 'A':
-                return 'task_a'
-            elif selected_item in ['B','C']:
-                return ['task_b','task_c']
+    def select_random():
+        import random
 
+        item_lst = ['A','B','C']
+        selected_item = random.choice(item_lst)
+        if selected_item == 'A':
+            return 'task_a'
+        elif selected_item in ['B','C']:
+            return ['task_b','task_c']
     
-    custom_branch_operator = CustomBranchOperator(task_id='python_branch_task')
-
+    python_branch_task = BranchPythonOperator(
+        task_id='python_branch_task',
+        python_callable=select_random
+    )
     
     def common_func(**kwargs):
         print(kwargs['selected'])
@@ -46,4 +45,4 @@ with DAG(
         op_kwargs={'selected':'C'}
     )
 
-    custom_branch_operator >> [task_a, task_b, task_c]
+    python_branch_task >> [task_a, task_b, task_c]
