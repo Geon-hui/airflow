b1c15c97e1743dc97e066a67980f25ea5316aa26
Geonhui <3634lelee@dgu.ac.kr>
2025-05-17 12:45:54 +0900

    bash - python, python - bash xcom 실습

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -13,7 +13,7 @@ with DAG(
     bash_command="echo START && "
                  "echo XCOM_PUSHED "
                  "{{ ti.xcom_push(key='bash_pushed',value='first_bash_message') }} && "
-                 "echo COMPLETE"
+                 "echo COMPLETE" # echo도 return 값으로 간주
     )
 
     bash_pull = BashOperator(
@@ -21,7 +21,7 @@ with DAG(
         env={'PUSHED_VALUE':"{{ ti.xcom_pull(key='bash_pushed') }}",
             'RETURN_VALUE':"{{ ti.xcom_pull(task_ids='bash_push') }}"},
         bash_command="echo $PUSHED_VALUE && echo $RETURN_VALUE ",
-        do_xcom_push=False
+        do_xcom_push=False # do_xcom_push=False로 설정하면 return 값을 xcom에 저장하지 않음
     )
 
     bash_push >> bash_pull
