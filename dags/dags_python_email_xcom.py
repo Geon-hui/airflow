7d8956b6469c3cee07b58f148e5eb22ebd60f695
Geonhui <3634lelee@dgu.ac.kr>
2025-05-17 14:51:51 +0900

    dags_id 수정

--- dags/dags_python_email_xcom.py
+++ dags/dags_python_email_xcom.py
@@ -4,7 +4,7 @@ import pendulum
 from airflow.decorators import task
 
 with DAG(
-    dag_id="dags_python_email_operator",
+    dag_id="dags_python_email_xcom",
     schedule="0 8 1 * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
