0c37e00b53475ff0746e3a77ceb99b4021d38191
Geonhui <3634lelee@dgu.ac.kr>
2025-05-19 21:36:14 +0900

    version upgrade

--- dags/dags_simple_http_operator.py
+++ dags/dags_simple_http_operator.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.decorators import task
 
 import pendulum
-from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.http.operators.http import HttpOperator
 
 
 with DAG(
@@ -14,7 +14,7 @@ with DAG(
 ) as dag:
 
     '''서울시 공공자전거 대여소 정보'''
-    tb_cycle_station_info = SimpleHttpOperator(
+    tb_cycle_station_info = HttpOperator(
         task_id='tb_cycle_station_info',
         http_conn_id='openapi.seoul.go.kr',
         endpoint='{{var.value.apikey_openapi_seoul_go_kr}}/json/tbCycleStationInfo/1/10/', # openapi 값을 variable => 노출 X, 다른 DAG에서 바뀌어도 전역변수 값바꿔서 일괄적용 가능능
